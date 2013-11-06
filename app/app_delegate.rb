c6865156fd51bce3e1d93badf209c453cdc70a09
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-11-06 21:29:39 +0100

    Now fetches value from MtGox

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,13 +1,13 @@
 class AppDelegate
 	def application(application, didFinishLaunchingWithOptions:launchOptions)
-		#BW::HTTP.get('http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast') do |response|
-		#	p response.body.to_str
-		#end
-		# UIScreen describes the display our app is running on
+		BW::HTTP.get('http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast') do |response|
+			json = BW::JSON.parse(response.body.to_str)
+			value = json['data']['last_local']['display_short']
 
-		@window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-		@window.makeKeyAndVisible
-		@window.rootViewController = BitcoinController.alloc.initWithNibName(nil, bundle: nil)
+			@window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+			@window.makeKeyAndVisible
+			@window.rootViewController = BitcoinController.alloc.initWithValue value
+		end
 
 		true
 	end
--- app/controllers/bitcoin_controller.rb
+++ app/controllers/bitcoin_controller.rb
@@ -1,14 +1,18 @@
 class BitcoinController < UIViewController
+	def initWithValue value
+		@value = value
+		init
+	end
+
 	def viewDidLoad
 		super
 
-		# self.view.backgroundColor = UIColor.whiteColor
 		self.view.backgroundColor = UIColor.colorWithPatternImage(UIImage.imageNamed('images/golden_gradient.jpg'))
 
 		@label = UILabel.alloc.initWithFrame(CGRectZero)
-		@label.text = '$262.40002'
+		@label.text = @value
 		@label.color = UIColor.whiteColor
-		@label.setFont(UIFont.systemFontOfSize(45))
+		@label.setFont(UIFont.systemFontOfSize(60))
 		@label.sizeToFit
 		@label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2 - 50)
 		self.view.addSubview @label
