eafa0416908223e9e8d706f42818e6c5baba1292
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-11-10 22:20:32 +0100

    Update value when activated

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,12 @@
 class AppDelegate
 	def application(application, didFinishLaunchingWithOptions:launchOptions)
+	end
+
+	def applicationDidBecomeActive(application)
+		fetch_rate_and_load_controller
+	end
+
+	def fetch_rate_and_load_controller
 		BW::HTTP.get('http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast') do |response|
 			json = BW::JSON.parse(response.body.to_str)
 			value = json['data']['last_local']['display_short']
@@ -11,4 +18,5 @@ class AppDelegate
 
 		true
 	end
+
 end
